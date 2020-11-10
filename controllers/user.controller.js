bf60dd26df247ae9009ba000f8ebc416ed37a896
omar salas rodriguez <servidorlocal@outlook.com>
2020-11-10 11:16:45 -0600

    crud completado

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -37,4 +37,58 @@ users.getAllUsers = (req, res) => {
     res.json(usersData);
 };
 
+users.createUser = (req, res) => {
+  const body = req.body;
+
+  // Validación de campos requeridos
+  if (!body.email || !body.password || !body.role) {
+    res.status(400).send('Ingresa todas las propiedades');
+  }
+
+  const userFound = usersData.find((user) => user.email === body.email);
+  console.log(userFound);
+  if (userFound) {
+    res.status(400).send('El usuario ya esta registrado');
+  } else {
+    const user = {
+      email: body.email,
+      password: body.password,
+      role: body.role,
+      id: usersData[usersData.length -1].id + 1,
+    };
+    usersData.push(user);
+    res.json(user);
+  }
+}
+
+users.deleteUser = (req, res) => {
+  const { userId } = req.params;
+  const index = usersData.findIndex((user) => user.id === +userId);
+  if (index === -1) {
+    res.status(400).send({error: 'Usuario no encontrado'});
+  } else {
+    usersData.splice(index, 1);
+    res.json({message: 'Usuario eliminado'});
+  }
+}
+
+users.updateUser = (req, res) => {
+  const { userId } = req.params;
+  const body = req.body;
+
+  const index = usersData.findIndex((user) => user.id === +userId);
+  if (index === -1) {
+    res.status(400).send({error: 'Usuario no encontrado'});
+  } else {
+    const user = {
+      ...usersData[index],
+      ...body,
+    };
+    usersData[index] = user;
+    res.json(user);
+  }
+  
+}
+
+
 module.exports = users;
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getAllUsers);
+router.post('/', userController.createUser);
+router.delete('/:userId', userController.deleteUser);
+router.patch('/:userId', userController.updateUser);
 
 module.exports = router;
